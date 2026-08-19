ff54f61049b36eda27eea711d6c2ccc1e7b3c40f
wzx0225 <3082353359@qq.com>
2026-08-19 11:34:31 +0800

    Fix import quotes in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   base: '/ht',
   site: "https://wzx0225.github.io",
+  trailingSlash: "always",
   integrations: [sitemap()],
 });
