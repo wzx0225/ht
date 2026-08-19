b8659aedfded77d90d9368c84390f6f629895493
wzx0225 <3082353359@qq.com>
2026-08-19 13:41:01 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   base: '/ht',
-  site: "https://wzx0225.github.io",
+  site: "https://wzx0225.github.io/ht",
   trailingSlash: "always",
   integrations: [sitemap()],
 });
